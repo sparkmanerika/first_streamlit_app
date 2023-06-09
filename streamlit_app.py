5ca9d9ef7a2e472bc3b6fc3e3ff5c23da529140a
sparkmanerika <130163861+sparkmanerika@users.noreply.github.com>
2023-06-09 08:43:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #DISPLAY UNI LIST CHOICE IN TABLE
 streamlit.dataframe(fruits_to_show)
 
+
+#CREATE THE REPEATABLE CODE BLOCK (CALLED A FUNCTION)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)     
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 #IMPORT FRUITY VICE DATA
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -33,10 +40,9 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)     
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-    
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+        
 except URLError as e:   
   streamlit.error()  
   
