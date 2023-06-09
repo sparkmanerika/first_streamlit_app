e0b3d6df55f56e115e1238ef3f8fc3b646e46ae0
sparkmanerika <130163861+sparkmanerika@users.noreply.github.com>
2023-06-09 08:20:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
+#DISPLAY BREAKFAST MENU
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
@@ -14,25 +12,34 @@ streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
 
+#BUILD YOUR OWN SMOOTHIE
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+#IMPORT FRUIT LIST FROM UNI LIST
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#CREATE A PICK LIST FROM UNI LIST
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
+#DISPLAY UNI LIST CHOICE IN TABLE
 streamlit.dataframe(fruits_to_show)
+
+#IMPORT FRUITY VICE DATA
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response.json())
 
+#DISPLAY FRUITY VICE CHOICE IN TABLE
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-# write your own comment - normalize
+#NORMALIZE THE DATA FOR DISPLAY
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - present in dataframe
+
+#DISPLAY THE DATAFRAME
 streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.stop()
 
